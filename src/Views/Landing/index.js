c225f30f4a4795ed24c2d13e8832942c13a31d4f
unknown <oliverbth05@gmail.com>
2019-03-28 22:52:11 -0400

    c9 Push

--- src/Views/Landing/index.js
+++ src/Views/Landing/index.js
@@ -16,72 +16,72 @@ import babel from '../../assets/Logos/babel-10.svg';
 const About = () => {
     return (
         <div>
-            <div className = 'container-flex-center bg-gradient'>
+            <div className='container-flex-center bg-gradient'>
                 <div>
-                    <h1 className = 'logo color-white text-center'>Rag</h1>
-                    <Link className = 'btn btn-primary btn-small m-a-2 color-pulse' to = '/login'>Get Started</Link>
+                    <h1 className='logo color-white text-center'>Rag</h1>
+                    <Link className='btn btn-primary btn-small m-a-2 color-pulse' to='/login'>Get Started</Link>
                 </div>
             </div>
 
-            <div className = 'container-flex-center bg-white'>
-                <div className = 'container-1000-mauto'>
+            <div className='container-flex-center bg-white'>
+                <div className='container-1000-mauto'>
 
-                <section className = 'p-a-3'>
-                    <h2 className = 'p-b-1 text-center m-b-1'>About</h2>
-                    <h4 className = 'p-t-1'>Features</h4>
-                    <ul>
-                        <li className = ' font-light'>User authentication via web-tokens.</li>
-                        <li className = ' font-light'>Written-content creation with in-built styling.</li>
-                        <li className = ' font-light'>User profiles with favorites and subscriptions.</li>
-                        <li className = ' font-light'>View, like and comment functionality.</li>
-                        <li className = ' font-light'>Viewable on all screen sizes.</li>
-                    </ul>
+                    <section className='p-a-3'>
+                        <h2 className='p-b-1 text-center m-b-1 font-normal'>About</h2>
+                        <h4 className='p-t-1 font-normal'>Features</h4>
+                        <ul>
+                            <li className=' font-light'>User authentication via web-tokens.</li>
+                            <li className=' font-light'>Written-content creation with in-built styling.</li>
+                            <li className=' font-light'>User profiles with favorites and subscriptions.</li>
+                            <li className=' font-light'>View, like and comment functionality.</li>
+                            <li className=' font-light'>Viewable on all screen sizes.</li>
+                        </ul>
 
-                    <h4 className = 'p-t-1 m-b-1 '><i class="fab fa-github"></i> GitHub</h4>
-                    <div className = 'logo-flex'>
-                        <a className = 'github-link m-r-s'>Client</a>
-                        <a className = 'github-link'>API</a>
-                    </div>
-
-                </section>
-
-
-
-                <section className = 'p-a-3'>
-                    <h2 className = 'p-b-1  text-center' >Technologies</h2>
-                    <div className = 'logo-grid'>
-                        <div>
-                            <h4 className = 'p-a-1 text-center '>Front-End</h4>
-                            <div className = 'logo-flex'>
-                                <Logo src = {react} name = 'React'/>
-                                <Logo src = {redux} name = 'Redux' />
-                                <Logo src = {reactRouter} name = 'React-Router' />
-                                <Logo src = {sass} name = 'Sass'/>
-                            </div>
+                        <h4 className='p-t-1 m-b-1 font-normal '><i class="fab fa-github"></i> GitHub</h4>
+                        <div className='logo-flex'>
+                            <a className='github-link m-r-s'>Client</a>
+                            <a className='github-link'>API</a>
                         </div>
 
-                        <div>
-                            <h4 className = 'p-a-1 text-center '>Back-End</h4>
-                            <div className = 'logo-flex'>
-                                <Logo src = {nodejs} name = 'Node'/>
-                                <Logo src = {express} name = 'Express' />
-                                <Logo src = {mongodb} name = 'MongoDB'/>
-                            </div>
-                        </div>
+                    </section>
 
-                        <div>
-                            <h4 className = 'p-a-1 text-center  '>Development</h4>
-                            <div className = 'logo-flex'>
-                                <Logo src = {git} name = 'Git'/>
-                                <Logo src = {webpack} name = 'Webpack' />
-                                <Logo src = {babel} name = 'Babel'/>
+
+
+                    <section className='p-a-3'>
+                        <h2 className='p-b-1  text-center font-normal' >Technologies</h2>
+                        <div className='logo-grid'>
+                            <div>
+                                <h4 className='p-a-1 text-center font-normal '>Front-End</h4>
+                                <div className='logo-flex'>
+                                    <Logo src={react} name='React' />
+                                    <Logo src={redux} name='Redux' />
+                                    <Logo src={reactRouter} name='React-Router' />
+                                    <Logo src={sass} name='Sass' />
+                                </div>
+                            </div>
+
+                            <div>
+                                <h4 className='p-a-1 text-center font-normal '>Back-End</h4>
+                                <div className='logo-flex'>
+                                    <Logo src={nodejs} name='Node' />
+                                    <Logo src={express} name='Express' />
+                                    <Logo src={mongodb} name='MongoDB' />
+                                </div>
+                            </div>
+
+                            <div>
+                                <h4 className='p-a-1 text-center font-normal  '>Development</h4>
+                                <div className='logo-flex'>
+                                    <Logo src={git} name='Git' />
+                                    <Logo src={webpack} name='Webpack' />
+                                    <Logo src={babel} name='Babel' />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </section>
+                    </section>
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 
--- src/api.js
+++ src/api.js
@@ -2,9 +2,12 @@ import axios from 'axios';
 import store from './store';
 
 const server = axios.create({
-  baseURL: 'https://angular-oliverbth05.c9users.io:8081',
+  baseURL: 'http://localhost:8081',
   headers: {
     'Content-Type': 'application/json',
   }
 })
 export default server;
+
+
+//https://angular-oliverbth05.c9users.io:8081
